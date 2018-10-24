e3e7a80fd4e1abddd1785b5045aad6b065fb5e5a
Kevin Durbin <kevindurb@gmail.com>
2018-10-24 13:44:17 -0600

    lay load component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Suspense } from 'react';
-import Thing from './Thing';
 
-// const Thing = React.lazy(() => import('./Thing'));
+const Thing = React.lazy(() => import('./Thing'));
 
 export default React.memo(() => (
   <Suspense fallback={<div>LOADING</div>}>
