d82603f5d4b062e5c85a30eeee88e89f6ececdf9
Kevin Durbin <kevindurb@gmail.com>
2018-10-24 18:58:07 -0600

    enhance createResource

--- src/Thing.js
+++ src/Thing.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { createResource } from './resource';
-const resource = createResource();
+
+const resource = createResource((id) => (
+  fetch(`http://localhost:3001/people/${id}`)
+    .then(res => res.json())
+));
 
 export default React.memo(({ id }) => {
-  const person = resource.read(`http://localhost:3001/people/${id}`);
+  const person = resource.read(id);
   return (
     <div key={person.id}>{person.name}</div>
   );
--- src/resource.js
+++ src/resource.js
@@ -1,53 +1,88 @@
-const cache = {
-};
+const RESOLVED = Symbol('RESOLVED');
+const PENDING = Symbol('PENDING');
+const NOT_STARTED = Symbol('NOT_STARTED');
+const REJECTED = Symbol('REJECTED');
 
-const getCache = (key) => (cache[key] || {
-  status: 'NOT_STARTED',
-  value: null,
-});
+export const createResource = (fn) => {
+  const cache = {};
 
-const setCache = (key, value) => {
-  cache[key] = value;
-}
+  const createCacheKey = JSON.stringify;
 
-const setPending = (key) => {
-  const result = getCache(key);
-  setCache(key, {
-    ...result,
-    status: 'PENDING',
-  })
-};
+  const getCache = (key) => (cache[key] || {
+    status: NOT_STARTED,
+    value: null,
+  });
 
-const setSuccess = (key, value) => {
-  const result = getCache(key);
-  setCache(key, {
-    ...result,
-    status: 'SUCCESS',
-    value,
-  })
-};
-
-export const createResource = () => ({
-  read(path) {
-    const result = getCache(path);
-    switch (result.status) {
-      case 'NOT_STARTED':
-        setPending(path)
-        throw fetch(path)
-          .then(r => r.json())
-          .then((data) => {
-            setSuccess(path, data);
-            return data;
-          });
-      case 'PENDING':
-        return undefined;
-      case 'SUCCESS':
-        return result.value;
-      default:
-        return undefined;
-    }
-  },
-  invalidate(path) {
-    setCache(path, undefined);
+  const setCache = (key, value) => {
+    cache[key] = value;
   }
-});
+
+  const setPending = (key, thenable) => {
+    const result = getCache(key);
+    setCache(key, {
+      ...result,
+      status: PENDING,
+      value: thenable,
+    })
+  };
+
+  const setResolved = (key, value) => {
+    const result = getCache(key);
+    setCache(key, {
+      ...result,
+      status: RESOLVED,
+      value,
+    });
+  };
+
+  const setRejected = (key, error) => {
+    const result = getCache(key);
+    setCache(key, {
+      ...result,
+      status: REJECTED,
+      value: error,
+    });
+  };
+
+  return {
+    read(...args) {
+      const key = createCacheKey(args);
+      const result = getCache(key, cache);
+      switch (result.status) {
+        case NOT_STARTED: {
+          const thenable = Promise.resolve(fn(...args))
+            .then((data) => {
+              setResolved(key, data);
+              return data;
+            })
+            .catch((error) => {
+              setRejected(key, error);
+              return error;
+            });
+          setPending(key, thenable);
+          throw thenable;
+        }
+        case PENDING: {
+          const thenable = result.value;
+          return thenable;
+        }
+        case RESOLVED: {
+          const value = result.value;
+          return value;
+        }
+        case REJECTED: {
+          const error = result.value;
+          return error;
+        }
+        default: {
+          // should be unreachable
+          return undefined;
+        }
+      }
+    },
+    invalidate(...args) {
+      const key = createCacheKey(args);
+      setCache(key, undefined);
+    }
+  };
+};
